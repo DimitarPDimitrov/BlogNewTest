01467fe0af77161530b9d7038163f52b35d419d4
DimitarPDimitrov <dimitar.p.dimitrov@gmail.com>
2017-05-06 16:10:49 +0300

    Change of BrowserHost & add GeckoDriver

--- Blog-Sceleton/Blog.UI.Tests/BrowserHost.cs
+++ Blog-Sceleton/Blog.UI.Tests/BrowserHost.cs
@@ -7,15 +7,16 @@ namespace Blog.UI.Tests
     {
         public static readonly SelenoHost Instance = new SelenoHost();
         //public static readonly string RootUrl;
-        private static readonly string RootUrl = "http://localhost:60634";
 
         static BrowserHost()
         {
-            //Instance.Run("Blog", 60639);
-            Instance.Run("Blog", 60634, w => w.WithRemoteWebDriver(() => new ChromeDriver()));
+            Instance.Run("Blog", 60639, config => config.WithRouteConfig(RouteConfig.RegisterRoutes));
+            //Instance.Run("Blog", 60639, w => w.WithRemoteWebDriver(() => new ChromeDriver()));
 
-           // RootUrl = Instance.Application.Browser.Url;
-            
+            //RootUrl = Instance.Application.Browser.Url;
+            //RootUrl = @"http://localhost:60634/";
+
+
         }
     }
 }
