c771b68b56ef9155f51b7540a72ae21b73d0bb24
DimitarPDimitrov <dimitar.p.dimitrov@gmail.com>
2017-05-06 15:26:22 +0300

    Change RootURL

--- Blog-Sceleton/Blog.UI.Tests/BrowserHost.cs
+++ Blog-Sceleton/Blog.UI.Tests/BrowserHost.cs
@@ -6,14 +6,15 @@ namespace Blog.UI.Tests
     class BrowserHost
     {
         public static readonly SelenoHost Instance = new SelenoHost();
-        public static readonly string RootUrl;
+        //public static readonly string RootUrl;
+        private static readonly string RootUrl = "http://localhost:60634";
 
         static BrowserHost()
         {
             //Instance.Run("Blog", 60639);
             Instance.Run("Blog", 60634, w => w.WithRemoteWebDriver(() => new ChromeDriver()));
 
-            RootUrl = Instance.Application.Browser.Url;
+           // RootUrl = Instance.Application.Browser.Url;
             
         }
     }
