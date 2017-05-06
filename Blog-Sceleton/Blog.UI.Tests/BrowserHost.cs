2cd425ebb74c8dfb0eed426d73185041392dc683
DimitarPDimitrov <dimitar.p.dimitrov@gmail.com>
2017-05-06 09:16:23 +0300

    Initial commit

---
+++ Blog-Sceleton/Blog.UI.Tests/BrowserHost.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium.Chrome;
+using TestStack.Seleno.Configuration;
+
+namespace Blog.UI.Tests
+{
+    class BrowserHost
+    {
+        public static readonly SelenoHost Instance = new SelenoHost();
+        public static readonly string RootUrl;
+
+        static BrowserHost()
+        {
+            Instance.Run("Blog", 60639);
+           // Instance.Run("Blog", 60639, w => w.WithRemoteWebDriver(() => new ChromeDriver()));
+
+            RootUrl = Instance.Application.Browser.Url;
+            
+        }
+    }
+}
---
+++ Blog-Sceleton/Blog.UI.Tests/TestClass.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace Blog.UI.Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void ShouldLoadBlog()
+        {
+            IWebDriver driver = BrowserHost.Instance.Application.Browser;
+            // WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(180));
+
+            driver.Navigate().GoToUrl(BrowserHost.RootUrl);
+            var logo = driver.FindElement(By.XPath("/html/body/div[1]/div/div[1]/a"));
+
+            Assert.AreEqual("SOFTUNI BLOG", logo.Text);
+        }
+    }
+}
