90ebe59950d9c5e420b3062d3be34c0999ee5704
DimitarPDimitrov <dimitar.p.dimitrov@gmail.com>
2017-05-08 16:26:45 +0300

    new test for navigation to blog post

--- Blog-Sceleton/Blog.UI.Tests/TestClass.cs
+++ Blog-Sceleton/Blog.UI.Tests/TestClass.cs
@@ -41,5 +41,18 @@ namespace Blog.UI.Tests
 
             Assert.AreEqual(@"http://localhost:10639/Article/List", browserUrl);
         }
+
+        [Test]
+        public void NavigateToBlogPostEdit()
+        {
+            IWebDriver driver = BrowserHost.Instance.Application.Browser;
+
+            //  driver.Navigate().GoToUrl(BrowserHost.RootUrl);
+            var browserUrl = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[2]/article/header/h2/a"));
+            browserUrl.Click();
+            var postHead = driver.FindElement(By.ClassName("btn btn-success btn-xs"));
+
+            Assert.AreEqual(@"Edit", postHead.Text);
+        }
     }
 }
